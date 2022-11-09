78180db185de2adda2e3504ca4c51145acb00566
MamunurRahmanMoon <mamunurrahmanmoon@gmail.com>
2022-11-09 15:43:14 +0600

    added first file

--- Lab-6_PHP_Database/index.php
+++ Lab-6_PHP_Database/index.php
@@ -7,6 +7,48 @@
     <title>Document</title>
 </head>
 <body>
-    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <fieldset>
+            <legend>Add Product</legend>
+            <table>
+                <tr>
+                    <td>
+                    Name
+                    <input type="text">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Buying Price
+                        <input type="text">
+                    </td>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Selling Price
+                        <input type="text">
+                    </td>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="checkbox" name="display" id="">Display
+                    </td>
+                </tr>
+            </table>
+        </fieldset>
+    </form>
+</body>
+</html>
 </body>
 </html>
